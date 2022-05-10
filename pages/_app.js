715242bcaa36537974dbed575051673d8b4f8fc0
Albar00n <mgrushi73@gmail.com>
2022-05-10 22:50:59 +0400

    style

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,10 @@
 import '../styles/globals.css'
-// import { createTheme, ThemeProvider, useTheme } from '@material-ui/core/styles';
-// import useMediaQuery from '@material-ui/core/useMediaQuery';
-// import { LanguageContextProvider } from "../context/languageContext"
-// function MyComponent() {
-//   const theme = useTheme();
-//   const matches = useMediaQuery(theme.breakpoints.up('sm'));
-
-//   return <span>{`theme.breakpoints.up('sm') matches: ${matches}`}</span>;
-// }
-// const theme = createTheme();
 
 
 function MyApp({ Component, pageProps }) {
 
   return (
-
-     <>
-
-
-        {/* <LanguageContextProvider>
-     <ThemeProvider theme={theme}> */}
         <Component {...pageProps} />
-        {/* </ThemeProvider>
-      </LanguageContextProvider> */}
-     </>
-
   )
 }
 
