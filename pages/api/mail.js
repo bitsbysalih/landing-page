a9c0ff7d75951e9fc6cf6c5adda95731b1371cea
Albar00n <mgrushi73@gmail.com>
2022-05-22 22:09:23 +0400

    mail

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -9,23 +9,16 @@ try{
   from:"noreply@beyin.me",
   to: "mgurshi73@gmail.com",
   subject:`Have new message:${req.body.name}`,
-  html:`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  html:`<!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-  <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
+    <title>Message</title>
   </head>
 <body>
-<div style={{display:'flex',justifyContent: 'center';alignItems:'center';borderRadius:'5px';overFlow:'hidden';font-family: 'helvetica', 'ui-sans';}}>
-<h3>You've got a new mail from ${req.body.name}, their email is: ✉️${req.body.email} </h3>
-<div style="font-size: 16px;">
+<div>
+<h3>You've got a new mail from ${req.body.name},<br /> their email is: ✉️${req.body.email} </h3>
+<div>
 <h3>Phone :${req.body.phone}</h3>
 <p>Message:</p>
 <p>${req.body.message}</p>
