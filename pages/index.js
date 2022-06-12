0e1d94b4c34e6753c258d70606d54ad638c6672a
Albar00n <mgrushi73@gmail.com>
2022-06-12 20:46:09 +0400

    aa

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Beyin</title>
+        <title>Landing Beyin</title>
+      	<meta charset="utf-8" />
+	    	<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
         <meta name="description" content="Beyin Digital Technology" />
          {/* <meta name="description" content="Watch TV shows and movies online. Stream TV episodes of South Park, Empire, SNL, Modern Family and popular movies on your favorite devices. Start your free trial now. Cancel anytime." /> */}
-         <meta data-rh="true" name="google" content="notranslate" />
+         <meta data-rh="true" name="google" content="translate" />
          <meta data-rh="true" name="keywords" content="Businesses card, virtual business card, online business card" />
          <meta data-rh="true" name="keywords" content="Augmented Reality business card,AR businesses card,VR businesses card,Virtual Reality Business card,Extended reality business card" />
          <meta data-rh="true" name="robots" content="index, follow"/>
