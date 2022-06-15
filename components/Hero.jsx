68fbe180c2aaac8fa0694298b982e96e017beed0
Albar00n <mgrushi73@gmail.com>
2022-06-15 16:02:35 +0400

    aa

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,53 +8,57 @@ import styles from "../styles/hero.module.css"
 export default function Hero() {
 
   return (
-    <div id="hero" className={styles.box}>
-      <Grid container direction="row" justifyContent="space-between">
-        <Grid item sm={7} md={6} lg={7}>
-          <div className={styles.boxx}>
-            <Typography    variant="h4" className={styles.typo}>
-              Get the most effective business
-              card ever for only 1$/month
-            </Typography>
-            <Typography
-                className={styles.typo2}
-              variant="body1"
-              style={{ margin: "2rem 1px"}}>
-              Extended Business Card Platform allows you to create extended
-              business cards for you, your company, or clients in no time and
-              without complex steps for as low as $1 per month!
-            </Typography>
-            {/* button */}
-          <Link href="https://ebcdb.beyin.me/signup">
-          <a className={styles.link}>
-          <StyleButton />
-          </a>
-          </Link>
-          </div>
-        </Grid>
-        {/* video */}
-        <Grid
-        item
-          container
-          alignItems="center"
-          justifyContent="center"
-          sm={5}
-          md={4}
-          lg={3}
-        >
-          <Grid className={styles.grid}>
-            <video
-              controls
-              autostart="true"
-              autoPlay
-              src="./video.mp4"
-              type="video/mp4"
-              alt="Beyin Video"
-              className={styles.video}
-            />
-          </Grid>
-        </Grid>
-      </Grid>
-    </div>
-  );
+		<div id="hero" className={styles.box}>
+			<Grid container direction="row" justifyContent="space-between">
+				<Grid item sm={7} md={6} lg={7}>
+					<div className={styles.boxx}>
+						<Typography
+							variant="h4"
+							className={styles.typo}
+							
+						>
+							Get the most effective business card ever for only 1$/month
+						</Typography>
+						<Typography
+							className={styles.typo2}
+							variant="body1"
+							style={{ margin: "2rem 1px" }}
+						>
+							Extended Business Card Platform allows you to create extended
+							business cards for you, your company, or clients in no time and
+							without complex steps for as low as $1 per month!
+						</Typography>
+						{/* button */}
+						<Link href="https://ebcdb.beyin.me/signup">
+							<a className={styles.link}>
+								<StyleButton />
+							</a>
+						</Link>
+					</div>
+				</Grid>
+				{/* video */}
+				<Grid
+					item
+					container
+					alignItems="center"
+					justifyContent="center"
+					sm={5}
+					md={4}
+					lg={3}
+				>
+					<Grid className={styles.grid}>
+						<video
+							controls
+							autostart="true"
+							autoPlay
+							src="./video.mp4"
+							type="video/mp4"
+							alt="Beyin Video"
+							className={styles.video}
+						/>
+					</Grid>
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,42 @@ import Container from "@material-ui/core/Container";
 import Loading from "../components/loading/loading";
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Landing Beyin</title>
-      	<meta charset="utf-8" />
-	    	<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-        <meta name="description" content="Beyin Digital Technology" />
-         {/* <meta name="description" content="Watch TV shows and movies online. Stream TV episodes of South Park, Empire, SNL, Modern Family and popular movies on your favorite devices. Start your free trial now. Cancel anytime." /> */}
-         <meta data-rh="true" name="google" content="translate" />
-         <meta data-rh="true" name="keywords" content="Businesses card, virtual business card, online business card" />
-         <meta data-rh="true" name="keywords" content="Augmented Reality business card,AR businesses card,VR businesses card,Virtual Reality Business card,Extended reality business card" />
-         <meta data-rh="true" name="robots" content="index, follow"/>
-         <meta name="Linkedin:site" content="@beyin-tech" />
-         <meta name="instagram:site" content="@beyin.me" />
-          <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>
-        <Header />
-        <Container>
-          <Loading />
-        </Container>
-      </div>
-    </>
-  );
+		<>
+			<Head>
+				<title>Landing Beyin</title>
+				<meta charset="utf-8" />
+				<meta
+					name="google-site-verification"
+					content="gFgDtzR0i6ZwRv2Bu3dj-Odvct12dr19DfqLunnasUU"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+				/>
+				<meta name="description" content="Beyin Digital Technology" />
+				{/* <meta name="description" content="Watch TV shows and movies online. Stream TV episodes of South Park, Empire, SNL, Modern Family and popular movies on your favorite devices. Start your free trial now. Cancel anytime." /> */}
+				<meta data-rh="true" name="google" content="translate" />
+				<meta
+					data-rh="true"
+					name="keywords"
+					content="Businesses card, virtual business card, online business card"
+				/>
+				<meta
+					data-rh="true"
+					name="keywords"
+					content="Augmented Reality business card,AR businesses card,VR businesses card,Virtual Reality Business card,Extended reality business card"
+				/>
+				<meta data-rh="true" name="robots" content="index, follow" />
+				<meta name="Linkedin:site" content="@beyin-tech" />
+				<meta name="instagram:site" content="@beyin.me" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div>
+				<Header />
+				<Container>
+					<Loading />
+				</Container>
+			</div>
+		</>
+	);
 }
